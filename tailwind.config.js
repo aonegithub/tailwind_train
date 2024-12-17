dcc45d01b19eccea15b163516ab28c8d290c4c1e
A-One <aone0960@gmail.com>
2024-12-17 22:02:34 +0800

    jit mode class 隨使用者切換

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     mode:'jit',
-    darkMode:false,
+    darkMode:'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
