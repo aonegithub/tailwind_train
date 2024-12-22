631d7570c56250672f8ef6a1ebec7457a4724186
aone <aone0960@gmail.com>
2024-12-23 00:20:34 +0800

    新增 fb 版面 blade

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/fb-index.js',
-                'resources/css/fb.css',
+                'resources/js/app.js'
             ],
             refresh: true,
         }),
