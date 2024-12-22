e1eff011bf8bc1459db68527c8d174a149c708bb
A-One <aone0960@gmail.com>
2024-12-23 00:42:53 +0800

    test

--- resources/views/fb.blade.php
+++ resources/views/fb.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/css/fb.css', 'resources/js/app.js'])
+    @vite('resources/css/fb.css')
 
     <title>Document</title>
 </head>
@@ -328,6 +328,6 @@
 </main>
 
 
-@vite(['resources/js/fb-index.js', 'resources/js/app.js'])
+@vite('resources/js/fb-index.js')
 </body>
 </html>
